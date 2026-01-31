f78e5b990106ed1da6c16fea7a0b1926dbd7499c
My Computer <remzi.huseynli55@gmail.com>
2026-01-31 14:41:20 +0400

    Add Unity Project

---
+++ Penguin Climb Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Sağ-sol hərəkət
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
